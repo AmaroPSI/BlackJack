539627b1273001bb264f49b2a8da517e7c446870
formando <pedroneves1234@hotmail.com>
2018-06-08 11:35:46 +0100

    Base de dados

---
+++ SQL BasedeDados.sql
@@ -0,0 +1,12 @@
+/*DROP TABLE IF EXISTS Utilizadores;
+
+CREATE TABLE Utilizadores (
+  ID int Unsigned zerofill not null auto_increment,
+  username varchar(30),
+  email varchar(40),
+  senha varchar(30),
+  nomecompleto varchar(60),
+  datadenascimento varchar(30),
+  primary key(ID)
+) engine=InnoDB;*/
+
